89c4b303a44abae29f7eee96e30af1596f0d40a1
samim3ini <sjm111cia@gmail.com>
2025-03-20 09:20:20 +0300

    SC

--- src/services/employeeService.ts
+++ src/services/employeeService.ts
@@ -1,27 +1,28 @@
 import axios from 'axios';
 
-const API_URL = 'https://rjjunsawi4.execute-api.us-east-1.amazonaws.com/test/employees';
+const API_Emp_URL = 'https://rjjunsawi4.execute-api.us-east-1.amazonaws.com/test/employees';
+const API_ATT_URL = 'https://rjjunsawi4.execute-api.us-east-1.amazonaws.com/test/attendance';
 
 export const fetchEmployees = async () => {
-  return axios.get(API_URL);
+  return axios.get(API_Emp_URL);
 };
 
 export const addEmployee = async (employeeData: any) => {
-  return axios.post(API_URL, employeeData, {
+  return axios.post(API_Emp_URL, employeeData, {
     headers: { 'Content-Type': 'application/json' },
   });
 };
 
 export const updateEmployee = async (employeeID: string, updatedData: any) => {
-  return axios.put(`${API_URL}/${employeeID}`, updatedData, {
+  return axios.put(`${API_Emp_URL}/${employeeID}`, updatedData, {
     headers: { 'Content-Type': 'application/json' },
   });
 };
 
 export const deleteEmployee = async (employeeID: string) => {
-  return axios.delete(`${API_URL}/${employeeID}`);
+  return axios.delete(`${API_Emp_URL}/${employeeID}`);
 };
 
 export const fetchAttendanceRecords = async () => {
-    return axios.get(API_URL+'?type=attendance');
+    return axios.get(API_ATT_URL);
 };
